693505bca494b574c329f9a0667b7db97092066d
quangminh <lequangminh9768@gmail.com>
2026-03-24 22:21:36 +0700

    Feat: init coreiot task

---
+++ include/coreIotTask.h
@@ -0,0 +1,4 @@
+#include "global.h"
+#include "mqtt_client.h"
+
+void coreIotTask(void *pvParameters);
---
+++ src/coreIotTask.cpp
@@ -0,0 +1,7 @@
+#include "coreIotTask.h"
+
+void coreIotTask(void *pvParematers) {
+    LOG_INFO("Iot", "Core IoT task started");
+    while (1) {
+    }
+}
