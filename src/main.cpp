bd561ebfca414f7612a1f9606e55147fb39c0a6a
PhamGiaLuong <pham15032004@gmail.com>
2026-03-30 20:56:09 +0700

    Refactor: Update platformio.ini and main.cpp for improved task management and structure according to 2-class node architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,54 @@
+#include "esp_now_manager.h"
 #include "global.h"
 #include "hardware_manager.h"
-#include "lcd_display.h"
 #include "led_blinky.h"
+
+#ifdef SENSOR_NODE
+#include "lcd_display.h"
 #include "neo_pixel.h"
-#include "network_manager.h"
 #include "read_sensor.h"
 #include "tiny_ml.h"
+#endif
+
+#ifdef GATEWAY_NODE
+#include "core_iot.h"
+#include "network_manager.h"
 #include "web_server.h"
+#endif
 
 void setup() {
     Serial.begin(115200);
+
+    initGlobal();
+    loadConfigFromFlash();
+    initEspNow();
+
+#ifdef SENSOR_NODE
+    LOG_INFO("MAIN", "Booting SENSOR NODE...");
     delay(2000);
     Wire.begin(SENSOR_SDA_PIN, SENSOR_SCL_PIN);
     strip.begin();
     strip.show();
     delay(500);
-    initGlobal();
-    loadConfigFromFlash();
 
-    // Create tasks
-    // Task 1: Led blinky
-    xTaskCreate(sensorLedBlinkyTask, "SensorLedTask", 2048, NULL, 2, NULL);
-    xTaskCreate(gatewayLedBlinkyTask, "GatewayLedTask", 2048, NULL, 2, NULL);
-    // Task 2: RGB led
-    xTaskCreate(neopixelTask, "NeopixelTask", 2048, NULL, 1, NULL);
-    // Task 3: Sensor reading & LCD display
-    xTaskCreate(readSensorTask, "SensorTask", 4096, NULL, 2, NULL);
-    xTaskCreate(lcdDisplayTask, "LCDTask", 4096, NULL, 1, NULL);
-    // Task 4: Web server
-    xTaskCreate(networkTask, "Network_Task", 4096, NULL, 5, NULL);
-    xTaskCreate(webServerTask, "WebServer_Task", 8192, NULL, 3, NULL);
+    xTaskCreate(sensorLedBlinkyTask, "Led_SN", 2048, NULL, 1, NULL);
+    xTaskCreate(neopixelTask, "NeoPixel", 2048, NULL, 2, NULL);
+    xTaskCreate(readSensorTask, "Sensor", 4096, NULL, 3, NULL);
+    xTaskCreate(lcdDisplayTask, "LCD", 4096, NULL, 2, NULL);
+    xTaskCreate(tinyMlTask, "TinyML", 4096, NULL, 2, NULL);
+    xTaskCreate(espNowSensorTask, "EspNow_SN", 4096, NULL, 4, NULL);
+#endif
+
+#ifdef GATEWAY_NODE
+    LOG_INFO("MAIN", "Booting GATEWAY NODE...");
+
+    xTaskCreate(gatewayLedBlinkyTask, "Led_GW", 2048, NULL, 1, NULL);
+    xTaskCreate(networkTask, "Network", 4096, NULL, 4, NULL);
+    xTaskCreate(webServerTask, "WebServer", 8192, NULL, 3, NULL);
     xTaskCreate(buzzerTask, "Buzzer_Task", 2048, NULL, 4, NULL);
-    // Task 5: ML prediction
-    xTaskCreate(tinyMlTask, "TinyML_Task", 4096, NULL, 4, NULL);
-    // Task 6: Core IoT communication
-
-    LOG_INFO("SETUP", "System setup complete.");
+    xTaskCreate(coreIotTask, "CoreIoT", 4096, NULL, 2, NULL);
+    xTaskCreate(espNowGatewayTask, "EspNow_GW", 4096, NULL, 4, NULL);
+#endif
 }
 
 void loop() {}
