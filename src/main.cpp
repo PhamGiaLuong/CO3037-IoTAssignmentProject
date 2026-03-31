24371d120aa8b661579aff2009e46c254a4939f3
quangminh <lequangminh9768@gmail.com>
2026-04-01 01:14:15 +0700

    Feat: test on esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "core_iot.h"
+#include "espnow_protocol.h"
 #include "global.h"
 #include "hardware_manager.h"
 #include "lcd_display.h"
@@ -9,28 +11,32 @@
 void setup() {
     Serial.begin(115200);
     delay(2000);
-    Wire.begin(SENSOR_SDA_PIN, SENSOR_SCL_PIN);
-    strip.begin();
-    strip.show();
+    // Wire.begin(SENSOR_SDA_PIN, SENSOR_SCL_PIN);
+    // strip.begin();
+    // strip.show();
     delay(500);
     initGlobal();
     loadConfigFromFlash();
+    // initEspNowSystem();
 
     // Create tasks
     // Task 1: Led blinky
 
     // Task 2: RGB led
-    xTaskCreate(neopixelTask, "NeopixelTask", 2048, NULL, 1, NULL);
+    // xTaskCreate(neopixelTask, "NeopixelTask", 2048, NULL, 1, NULL);
     // Task 3: Sensor reading & LCD display
-    xTaskCreate(readSensorTask, "SensorTask", 4096, NULL, 2, NULL);
-    xTaskCreate(lcdDisplayTask, "LCDTask", 4096, NULL, 1, NULL);
+    // xTaskCreate(readSensorTask, "SensorTask", 4096, NULL, 6, NULL);
+    // xTaskCreate(lcdDisplayTask, "LCDTask", 4096, NULL, 1, NULL);
     // Task 4: Web server
-    xTaskCreate(networkTask, "Network_Task", 4096, NULL, 5, NULL);
-    xTaskCreate(webServerTask, "WebServer_Task", 8192, NULL, 3, NULL);
-    xTaskCreate(buzzerTask, "Buzzer_Task", 2048, NULL, 4, NULL);
+    // xTaskCreate(networkTask, "Network_Task", 4096, NULL, 5, NULL);
+    // xTaskCreate(webServerTask, "WebServer_Task", 8192, NULL, 3, NULL);
+    // xTaskCreate(buzzerTask, "Buzzer_Task", 2048, NULL, 4, NULL);
     // Task 5: ML prediction
 
     // Task 6: Core IoT communication
+    // xTaskCreate(espNowRxTask, "EspNowRxTask", 4096, NULL, 4, NULL);
+    // xTaskCreate(espNowTxTask, "EspNowTxTask", 4096, NULL, 4, NULL);
+    xTaskCreate(coreIotTask, "CoreIotTask", 4096, NULL, 3, NULL);
 
     LOG_INFO("SETUP", "System setup complete.");
 }
